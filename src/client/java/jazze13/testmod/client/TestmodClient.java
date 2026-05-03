e220d3919f99620a62b2fe940b8684d1664aa807
jazze13 <jazze.hang@gmail.com>
2026-05-03 03:46:27 +0300

    init

---
+++ src/client/java/jazze13/testmod/client/TestmodClient.java
@@ -0,0 +1,10 @@
+package jazze13.testmod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TestmodClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
